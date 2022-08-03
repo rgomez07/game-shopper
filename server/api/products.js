19ad7e37c7f25c8758e54f9981d146e4befe91db
Rigoberto Gomez <rigo@Rigobertos-MacBook-Air.local>
2022-08-03 18:31:35 -0500

    feat: user product put delete routes

--- server/api/products.js
+++ server/api/products.js
@@ -26,3 +26,24 @@ router.get('/:id', async (req, res, next) => {
     next(err);
   }
 });
+
+//Put Routes
+router.put('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    res.send(await product.update(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Delete Routes
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    await product.destroy();
+    res.send(product);
+  } catch (error) {
+    next(error);
+  }
+});
