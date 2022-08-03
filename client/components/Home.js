a4f861b6405d5c8bf6dc90aa918fce928cf3df8f
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-03 15:06:41 -0400

    pulling in all products component

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
+import AllProducts from "./AllProducts";
 
 /**
  * COMPONENT
  */
-export const Home = props => {
-  const {username} = props
+export const Home = (props) => {
+  const { username } = props;
 
   return (
-    <div>
-      <h3>Welcome, {username}</h3>
-    </div>
-  )
-}
+    <>
+      <div>
+        <h3>Welcome, {username}</h3>
+      </div>
+      <AllProducts />
+    </>
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    username: state.auth.username
-  }
-}
+    username: state.auth.username,
+  };
+};
 
-export default connect(mapState)(Home)
+export default connect(mapState)(Home);
