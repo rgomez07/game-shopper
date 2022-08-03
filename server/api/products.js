5ccbe9448a94fab213d6e6bb8c79b8348dc698ee
Rigoberto Gomez <rigo@Rigobertos-MacBook-Air.local>
2022-08-03 10:58:41 -0500

    added api routes for all products and single product

---
+++ server/api/products.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  models: { Product },
+} = require('../db');
+module.exports = router;
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+router.get('/:id', async (req, res, next) => {
+  try {
+    let productid = req.params.id;
+    let singleProduct = await Product.findByPk(productid);
+
+    res.send(singleProduct);
+  } catch (err) {
+    next(err);
+  }
+});
--- server/db/index.js
+++ server/db/index.js
@@ -1,18 +1,19 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require('./db');
 
-const User = require('./models/User')
-const Cart = require('./models/Cart')
+const User = require('./models/User');
+// const Cart = require('./models/Cart')
 
 //associations could go here!
 
-Cart.belongsTo(User)
-User.hasMany(Cart) // assumes we have multiple lines for each different product type for a user
+// Cart.belongsTo(User)
+// User.hasMany(Cart) // assumes we have multiple lines for each different product type for a user
 
 module.exports = {
   db,
   models: {
-    User, Cart
+    User,
+    // Cart
   },
-}
+};
