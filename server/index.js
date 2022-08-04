7c57c82bb073ca40e98fa94e5fbfbafa78e04ecc
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-04 17:39:23 -0400

    updating with console.log) ;

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const PORT = process.env.PORT || 8080
 const app = require('./app')
 const seed = require('../script/seed');
 
+console.log('this is from index.js')
+
+
 const init = async () => {
   try {
     if(process.env.SEED === 'true'){
@@ -14,7 +17,7 @@ const init = async () => {
     // start listening (and create a 'server' object representing our server)
     app.listen(PORT, () => console.log(`GAME SHOPPING ON PORT ${PORT}`))
   } catch (ex) {
-    console.log(ex)
+    console.log('ISSUE INITIATING THE SERVER',ex)
   }
 }
 
