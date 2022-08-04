46983b5b1e2a17ba3b9380526aef6a90b261c505
alexjcwong <alex.jc.wong@gmail.com>
2022-08-04 18:13:56 -0400

    first pass at add to cart route

--- server/api/orderProducts.js
+++ server/api/orderProducts.js
@@ -4,6 +4,7 @@ const {
 } = require("../db");
 module.exports = router;
 
+//get cart
 router.get("/:id", async (req, res, next) => {
   try {
     const userCart = await User.findOne({
@@ -31,5 +32,60 @@ router.get("/:id", async (req, res, next) => {
     }
   } catch (err) {
     next(err);
+  }
+});
+
+//add to cart
+router.put("/:id", async (req, res, next) => {
+  const { userId, productId } = req.body;
+  console.log("shfiaohguqeg>>>>>>>>", req.body);
+  try {
+    const userCart = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          // join it with corresponding open order
+          model: Order,
+          where: {
+            status: "open",
+          },
+          // join it with corresponding product(s)
+          include: [Product],
+        },
+      ],
+    });
+
+    let orderInCart = {};
+    let productList = userCart.orders[0].products;
+
+    if (userCart.orders.length) {
+      orderInCart = userCart.orders[0];
+
+      let productIndex = productList.findIndex((product) => {
+        product.id === req.body.id;
+      });
+
+      if (productIndex !== -1) {
+        userCart.orders[productIndex].quantity =
+          productList[productIndex].order_product.quantity + req.body.quantity;
+        productList[productIndex].order_product.unit_price = req.body.price;
+        productList[productIndex].total =
+          productList[productIndex].order_product.quantity * req.body.price;
+        productList[productIndex].subtotal = productList[productIndex]
+          .map((product) => product.total)
+          .reduce((accumulator, current) => accumulator + current);
+      }
+      // remainder of code goes here
+
+      res.send(req.body);
+    } else {
+      await Order.create({ status: "open" });
+    }
+
+    //parent.addChild()
+  } catch (error) {
+    next(error);
   }
 });
