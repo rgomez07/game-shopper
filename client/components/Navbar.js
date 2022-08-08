88e4ee39c46bcb8eab4ea48352aabd3c38dabd6d
Rigoberto <rigobertobgomez@gmail.com>
2022-08-08 17:45:13 -0500

    merged with savoy has merge conflict

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { FiShoppingCart } from 'react-icons/fi';
-<<<<<<< HEAD
 import { logout, me } from '../store/auth';
+import { FiShoppingCart } from 'react-icons/fi';
 export class Navbar extends React.Component {
   componentDidMount() {
     this.props.current(), this.props.handleClick();
@@ -14,41 +13,39 @@ export class Navbar extends React.Component {
     }
     return (
       <div>
-        <h1 className='pageTitle'>Game Shopper</h1>
+        <h1 className="pageTitle">Game Shopper</h1>
         <nav>
           {this.props.isLoggedIn ? (
             <div>
               {/* The navbar will show these links after you log in */}
-              <Link to='/home' className='textColor'>
+              <Link to="/home" className="textColor">
                 Home
               </Link>
               <a
-                href='#'
+                href="#"
                 onClick={this.props.handleClick}
-                className='textColor'>
+                className="textColor"
+              >
                 Logout
               </a>
               {this.props.userType === 'Admin' ? (
-                <Link to='/users' className='textColor'>
+                <Link to="/users" className="textColor">
                   Users
                 </Link>
               ) : null}
               <Link to={`/users/cart/${id}`} className="cartTextColor">
-            <FiShoppingCart />
-          </Link>
+                <FiShoppingCart />
+              </Link>
             </div>
           ) : (
             <div>
               {/* The navbar will show these links before you log in */}
-              <Link to='/login' className='textColor'>
+              <Link to="/login" className="textColor">
                 Login
               </Link>
-              <Link to='/signup' className='textColor'>
+              <Link to="/signup" className="textColor">
                 Sign Up
               </Link>
-              <Link to={`/users/cart/${id}`} className="cartTextColor">
-            <FiShoppingCart />
-          </Link>
             </div>
           )}
         </nav>
@@ -57,52 +54,6 @@ export class Navbar extends React.Component {
     );
   }
 }
-=======
-import { logout } from '../store';
-import { FiShoppingCart } from 'react-icons/fi';
-
-const Navbar = ({ handleClick, isLoggedIn, id }) => (
-  <div>
-    <Link to="/home" className="textColor">
-      <h1 className="pageTitle">Game Shopper</h1>
-    </Link>
-
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home" className="textColor">
-            Home
-          </Link>
-          <a href="#" onClick={handleClick} className="textColor">
-            Logout
-          </a>
-          <Link to="/users" className="textColor">
-            Users
-          </Link>
-          <Link to={`/users/${id}/profile`} className="textColor">
-            Profile
-          </Link>
-          <Link to={`/users/cart/${id}`} className="cartTextColor">
-            <FiShoppingCart />
-          </Link>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login" className="textColor">
-            Login
-          </Link>
-          <Link to="/signup" className="textColor">
-            Sign Up
-          </Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-);
->>>>>>> 51837830fdc77ceb1384b83df0c57c2b1bc8ee6b
 
 /**
  * CONTAINER
